c54d5b82d22fc19d709bd2d0a8b47c0c174405f2
Apurv Kulkarni <162367816+ApurvKulkarni05@users.noreply.github.com>
2025-07-21 13:25:17 +0530

    reverse_LL.cpp

---
+++ reverse_LL.cpp
@@ -0,0 +1,31 @@
+//here basically in this question the logic behind the is use stack concept because we stored nodes temporarily from original as we traverse it 
+//Traverse the linked list using a temporary variable `temp` till it reaches null. At each node, push the value at the current node onto the stack. 
+//Set variable `temp` back to the head of the linked list. While the stack is not empty, set the value at the temp node to the value at the top of the stack. Pop the stack and move temp to the next node till it reaches null
+//return karo head as a new reverseLL
+class Node{
+public:int data;
+Node*next;
+Node(int data1, Node *next1){
+data=data1;
+next=*next1;
+}
+Node(int data1){
+data=data1;
+next=NULL;
+}
+void reverseLinked_List(Node *head){
+  while(temp!=NULL){
+st.push(temp->data);
+temp=temp->next;
+  }
+temp=head;
+
+
+while(temp!=NULL){
+temp->data=st.top();
+st.pop();
+temp->next1;
+return head;
+
+
+//i used this logic 
